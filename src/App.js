8557397ea732663fdded2502809b5da97c0148c3
alex-sizova <aleks.sizova@gmail.com>
2020-06-29 23:28:07 +0300

    Basic component structure added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import {Route, Switch} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
+import QuizList from './containers/QuizList/QuizList'
+import Auth from './containers/Auth/Auth'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Route path="/auth" component={Auth} />
+      <Route path="/quiz-creator" component={QuizCreator} />
+      <Route path="/quiz/:id" component={Quiz} />
+      <Route path="/" component={QuizList} exact />
     </Layout>
   );
 }
